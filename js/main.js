7cc147bc1f4edc6140aa84f89c35eb604d6488ba
nastyakpi1995 <nastyakpi1995@gmail.com>
2020-09-07 19:01:37 +0300

    do tabs

--- js/main.js
+++ js/main.js
@@ -76,3 +76,19 @@ $(function() {
         $(".menu-list .accordion-toggle").not(jQuery(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
     });
 }); // jQuery load
+
+
+// tabs
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
